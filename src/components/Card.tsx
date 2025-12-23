dce8ff285f2bf38e812369104513bd4615d9d43f
DESKTOP-SN72L4C\BasPa <baspagie@hotmail.com>
2025-12-24 00:39:32 +0100

    Enhance Card component and PlayQuiz page with selection functionality

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,24 +1,27 @@
 import Button from "./Button";
 
 interface CardProps {
+  id: string;
   title: string;
   copy?: string;
   image?: string;
+  isSelected?: boolean;
   onSelect?: () => void;
 }
 
-const Card = ({ title, copy, image, onSelect }: CardProps) => {
+const Card = ({ id, title, copy, image, isSelected, onSelect }: CardProps) => {
   return (
-    <div className="bg-slate-800 p-6 rounded-lg shadow-md w-60 gap-4 flex flex-col items-center justify-between">
+    <div
+      id={`option${id}`}
+      className={`option ${
+        isSelected ? "bg-green-900" : "bg-slate-800"
+      } p-6 rounded-lg shadow-md w-60 gap-4 flex flex-col items-center justify-between`}
+    >
       <h2 className="text-2xl font-semibold">{title}</h2>
       <p className="text-lg font-normal">{copy}</p>
 
       {image && <img src={image} alt={title} className="mx-auto" />}
-      <Button
-        copy="Select"
-        variant="secondary"
-        onClick={onSelect || (() => console.log("Answer selected:", title))}
-      />
+      <Button copy="Select" variant="secondary" onClick={onSelect} />
     </div>
   );
 };
--- src/pages/PlayQuiz.tsx
+++ src/pages/PlayQuiz.tsx
@@ -3,8 +3,11 @@ import { Link } from "react-router-dom";
 import Button from "../components/Button";
 import Card from "../components/Card";
 import Quiz from "../data/sampleQuiz.ts";
+import { useState } from "react";
 
 const PlayQuiz = () => {
+  const [selectedAnswer, setSelectedAnswer] = useState(99);
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-neutral-900 text-white/90">
       <div className="max-w-screen-xl lg:wi mx-auto p-8 text-center flex flex-col gap-6">
@@ -15,9 +18,13 @@ const PlayQuiz = () => {
         <div className="px-3 flex justify-center gap-4 flex-wrap">
           {Quiz.questions[0].options.map((option, index) => (
             <Card
-              key={index}
+              id={String(index)}
               title={String.fromCharCode(65 + index)}
               copy={option}
+              isSelected={selectedAnswer === index}
+              onSelect={() => {
+                setSelectedAnswer(index);
+              }}
             />
           ))}
         </div>
